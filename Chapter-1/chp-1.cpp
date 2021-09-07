43ab0715282b3ab87c4fe7d719a1011637190e75
Ethan Souders <74029040+Xcomred@users.noreply.github.com>
2021-09-06 23:43:41 -0500

    Update chp-1.cpp

--- Chapter-1/chp-1.cpp
+++ Chapter-1/chp-1.cpp
@@ -1,1 +1,21 @@
+a.	Employee
+Employee is a struct data type that has the following attributes, or data members, or data fields
+
+struct Employee {
+string   	firstName;
+string	lastName;
+int		SSN;
+string	department;
+string	role;
+double	salary;
+};
+
+b.	Create an employee DB with 3 employees by using dynamic array allocation. You can do hardcoding the information about the employees. 
+c.	Define display() to display all the employee information on the console
+d.	Define get_avg_salary() to compute the average salary of employees
+e.	Release any dynamically allocated memory in your program without memory leak
+f.	In the output of your program, display the address of each employee, address of all the local variables, the information of employees in a tabular format and the average salary.
+g.	Apply indentations correctly to make your program readable.
+h.	No need of classes/objects or makefile
+i.	No need of makefile
 
