3ca309a44239e44679b033c4e1f715649638df4f
Ethan Souders <74029040+Xcomred@users.noreply.github.com>
2021-09-08 10:12:03 -0500

    Update chp-1.cpp

--- Chapter-1/chp-1.cpp
+++ Chapter-1/chp-1.cpp
@@ -19,3 +19,71 @@ g.	Apply indentations correctly to make your program readable.
 h.	No need of classes/objects or makefile
 i.	No need of makefile
 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Employee // Apprently Struct is the same as a class but with not private variables. First time using one they seem cool
+{
+    string  firstName;
+    string	lastName;
+    int		SSN = 0;
+    string	department;
+    string	role;
+    double	salary = 0;
+};
+
+void display(Employee data[]) // I may need to pass this by refrance
+{
+   
+    int x = 3;
+    for (int i = 0; i < x ; i++)
+    {
+        cout << data[i].firstName << endl;
+        cout << data[i].lastName << endl;
+        cout << data[i].SSN << endl;
+        cout << data[i].department << endl;
+        cout << data[i].role << endl;
+        cout << data[i].salary << endl;
+        if (i >= 0)
+        {
+            cout << endl << "Next Employee" << endl;
+        }
+
+    }
+}double get_avg_Salery(Employee data[])
+{
+    int i = 0;
+    double y;
+    double avg;
+    int x = 3;
+    for ( i < x; i++;)
+    {
+        y =+ data[i].salary;
+        
+    }
+    avg = y / i;
+
+    return avg;
+
+}
+int main()
+{
+    struct Employee data[3];
+
+    data[0] = { "Ethan", "Souders", 44312, "Math", "Tech", 54000 };
+    data[1] = { "Kaylee", "Souders", 443156, "English", "Teacher", 43000 };
+    data[2] = { "Evan", "Souders", 9995451, "Computer", "Tech", 60000 };
+    Employee* p = &data[3];
+
+    display(data);
+    get_avg_Salery(data);
+
+    //cout << (*p).firstName << endl; used as a test. I really should just use a reposirty 
+    delete p; // This deletes it from the stack 
+
+    return 0;
+}
+
+
